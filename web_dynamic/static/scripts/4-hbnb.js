d202afa874c231684d2ad4faf89e62b1fbc5ea03
Mvke-IT <michael.duarte09@gmail.com>
2024-05-31 16:10:05 -0400

    added node js to make button interactive

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,63 +1,65 @@
-$(document).ready(init);
+$(function () {
+  const amenityIds = {};
 
-const HOST = '0.0.0.0';
-const amenityObj = {};
+  $('input[type=checkbox]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-function init () {
-  $('.amenities .popover input').change(function () {
-    if ($(this).is(':checked')) {
-      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenityObj[$(this).attr('data-name')];
-    }
-    const names = Object.keys(amenityObj);
-    $('.amenities h4').text(names.sort().join(', '));
-  });
-
-  apiStatus();
-  searchPlacesAmenities();
-}
-
-function apiStatus () {
-  const API_URL = `http://${HOST}:5001/api/v1/status/`;
-  $.get(API_URL, (data, textStatus) => {
-    if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
+    if ($(this).prop('checked')) {
+      amenityIds[amenityId] = amenityName;
     } else {
-      $('#api_status').removeClass('available');
+      delete amenityIds[amenityId];
     }
+    console.log(amenityIds);
+    $('.amenities h4').text(Object.values(amenityIds).join(', '));
   });
-}
+});
 
-function searchPlacesAmenities () {
-  const PLACES_URL = `http://${HOST}:5001/api/v1/places_search/`;
-  $.ajax({
-    url: PLACES_URL,
-    type: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify({ amenities: Object.values(amenityObj) }),
-    success: function (response) {
-      $('SECTION.places').empty();
-      for (const r of response) {
-        const article = ['<article>',
-          '<div class="title_box">',
-        `<h2>${r.name}</h2>`,
-        `<div class="price_by_night">$${r.price_by_night}</div>`,
-        '</div>',
-        '<div class="information">',
-        `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
-        `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
-        `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
-        '</div>',
-        '<div class="description">',
-        `${r.description}`,
-        '</div>',
-        '</article>'];
-        $('SECTION.places').append(article.join(''));
-      }
-    },
-    error: function (error) {
-      console.log(error);
+  // Update API status
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
     }
   });
-}
+
+  // Function to load and display places
+  function loadAndDisplayPlaces () {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: Object.keys(amenityIds) }),
+      success: function (data) {
+        console.log(data);
+        $('section.places').empty(); // Clear current places
+        for (const place of data) {
+          const article = `<article>
+                      <div class="title_box">
+                          <h2>${place.name}</h2>
+                          <div class="price_by_night">${place.price_by_night}</div>
+                      </div>
+                      <div class="information">
+                          <div class="max_guest">${place.max_guest} Guest(s)</div>
+                          <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+                          <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+                      </div>
+                      <div class="description">
+                          ${place.description}
+                      </div>
+                  </article>`;
+          $('section.places').append(article);
+        }
+      }
+    });
+  }
+
+  // Load and display places on page load
+  loadAndDisplayPlaces();
+
+  // Handle search button click
+  $('button').click(function () {
+    console.log('Search button clicked');
+    loadAndDisplayPlaces(); // Load and display places based on selected amenities
+  });
